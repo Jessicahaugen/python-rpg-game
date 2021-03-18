c1cc80c3456169b53dfd4876c8cbea0f46d7245c
Jessica Haugen <j.rose.haugen@gmail.com>
2021-03-18 11:26:59 -0700

    push

--- pythonproject.py
+++ pythonproject.py
@@ -1,1 +1,2 @@
-Messsage = "Hail fellows well met, and welcome to the land of sanctity.  "
+Messsage = "Hail fellows well met, and welcome to the land of sanctity. Amongst all Ladies Lords and Knights we have lost a princess. Henceforth we ask of your intentions? is thou heart (G) Chivalrous or (D) Devilish? " 
+
