263e7d7f614c47dbe06be835eb33fc3a02ed2157
Nischal Pahari <techinicallynischal@gmail.com>
2021-03-18 12:44:01 -0400

    secondFramework

---
+++ pythonproject.py
@@ -0,0 +1,28 @@
+class Knight:
+    def __init__(self, name, age, hp, items, spell):
+        self.name = name
+        self.age = age
+        self.hp = hp
+        self.items = items
+        self.spell = spell
+    
+    def attack(self):
+        self.hp -= 1000
+        print("you have attacked")
+
+    def spell(self):
+        self.spell -= 100
+        print(f"You have attacked with {self.spell}")
+
+
+demon_knight = Knight("Demon Knight", 40, 1000, "Dark Excalibur", "a fireball")
+
+holy_knight = Knight("Holy Knight", 20, 10000, "Light Excalibur", "divine light")
+
+def welcome_messsage():
+    print("Welcome")
+
+welcome_messsage()
+print(demon_knight.hp)
+demon_knight.attack()
+print(demon_knight.hp)
