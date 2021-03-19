aa08bc1558f5c7299be9a27478a863f63d4c8902
Taylor Nicholas <taylor.drake1357@gmail.com>
2021-03-19 10:48:28 -0400

    "while loop"
    loopy

--- pythonproject.py
+++ pythonproject.py
@@ -1,5 +1,6 @@
 
-Messsage = "Hail fellows well met, and welcome to the land of sanctity. Amongst all Ladies Lords and Knights we have lost a princess. Henceforth we ask of your intentions? is thou heart (G) Chivalrous or (D) Devilish? " 
+Messsage = "Hail fellows well met, and welcome to the land of sanctity. Amongst all Ladies Lords and Knights we have lost a princess. Henceforth we ask of your intentions? is thou heart (G) Chivalrous or (D) Devilish? "
+
 
 class Knight:
     def __init__(self, name, age, hp, items, spell):
@@ -8,7 +9,7 @@ class Knight:
         self.hp = hp
         self.items = items
         self.spell = spell
-    
+
     def attack(self):
         self.hp -= 1000
         print("you have attacked")
@@ -20,12 +21,41 @@ class Knight:
 
 demon_knight = Knight("Demon Knight", 40, 1000, "Dark Excalibur", "a fireball")
 
-holy_knight = Knight("Holy Knight", 20, 10000, "Light Excalibur", "divine light")
+holy_knight = Knight("Holy Knight", 20, 10000,
+                     "Light Excalibur", "divine light")
+
 
 def welcome_messsage():
     print("Welcome")
+
 
 welcome_messsage()
 print(demon_knight.hp)
 demon_knight.attack()
 print(demon_knight.hp)
+
+
+def victoryMessage():
+    print("""
+    Congratz you have slain the dragon
+    """)
+
+
+while [demon_knight.hp, mage_Knigh.hp, holy_Knight.hp] > 0:
+    print("You've been burned to crisp!")
+
+    if()
+
+while dragon.hp > 0:
+
+    characterAttack(demond_Knight, Dragon)
+    if Dragon == 0:
+        victoryMessage()
+
+    characterAttack(mage_Knight, Dragon)
+    elif Dragon == 0:
+        victoryMessage()
+
+    characterAttack(holy_Knight, Dragon)
+    elif Dragon == 0:
+        victoryMessage()
